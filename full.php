414e45367eb54cef603af66b96688eca76292a64
long <lykonglong@qq.com>
2017-09-18 10:28:23 +0700

    Revert "do full mv page, modify(header,nav,trialer)"
    This reverts commit 23424c3e390394a98da1c3e9548ae9971f89ef6c.

--- full.php
+++ full.php
@@ -1,34 +1,42 @@
 <?php include  "includes/function.php" ?>
 <?php  include "includes/header.php"; ?>
-<?php /* include "includes/nav.php"; */?>
+<?php  include "includes/nav.php"; ?>
 
-<?php
 
-if(isset($_GET['movie'])){
-    $movie_id=$_GET['movie'];
-    $pre_st=$connection->prepare("select * from movies where movie_id=?");
-    $pre_st->bind_param("s",$movie_id);
-    $pre_st->execute();
-    $select_movie=$pre_st->get_result();
-    //$row=$select_video->num_rows;
-    while($row= $select_movie->fetch_assoc()){
-        $movie_full=$row['movie_full'];
-    }
-
-}
-?>
-
-    <main class="page__main main" style="padding-top:50px;">
+    <main class="page__main main">
         <!--VIDEO-->
         <div class="inner">
-            <div class="container">
-                <div class="info__inner">
-<!--                <div class="info__content">-->
-                        <video src="movie/full/<?php echo $movie_full; ?>" controls autoplay preload="auto"  width="100%" height="100%" controlsList="nodownload" id="myVideoFile"></video>
-<!--                </div>-->
+
+
+                <div class="container">
+                    <div class="info__inner">
+
+<!--                        <div class="info__content">-->
+
+
+                                    <video src="movie/trailer/BLACKPANTHER.MP4" controls autoplay preload="auto"  width="100%" height="100%" controlsList="nodownload" id="myVideoFile">
+
+                                    </video>
+
+
+
+
+
+
+              
+<!--                             </div>-->
+
+                    </div>
                 </div>
-            </div>
+
+
+          
+
+          
         </div>
         <!--/VIDEO-->
+
     </main>
+
+
 <?php  include "includes/footer.php"; ?>
