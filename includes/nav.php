90e9cff9afeadca80b3c293dbdc1f75bdacfa10b
long <lykonglong@qq.com>
2017-09-25 13:21:57 +0700

    fix logo

--- includes/nav.php
+++ includes/nav.php
@@ -8,7 +8,7 @@
         <div class="container">
             <div class="header__left">
                 <a href="<?php echo BASE_URL; ?>" class="logo js-ajax-link">
-                    <img src="images/logo1.svg" alt="" width="95px" height="70px">
+                    <img src="images/logo.png" alt="" width="95px" height="70px">
                 </a>
                 <nav class="header__nav">
                     <ul class="header__nav-list">
